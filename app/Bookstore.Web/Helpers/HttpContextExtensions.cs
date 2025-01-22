e39c8f201baa22a105486f52ed8e7090bd18fc10
NoReply Amazon <no-reply@amazon.com>
2025-01-22 15:10:20 +0000

    [Commit] Iteration 21 #errors = 0 <= 3.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Helpers/HttpContextExtensions.cs][CS0246][23,13] The type or namespace name 'HttpCookie' could not be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Helpers/HttpContextExtensions.cs
+++ app/Bookstore.Web/Helpers/HttpContextExtensions.cs
@@ -1,10 +1,5 @@
 using System;
-using System.Drawing;
-using System.Net;
-using Microsoft.AspNetCore.Owin;
-
 using Microsoft.AspNetCore.Http;
-
 
 namespace Bookstore.Web.Helpers
 {
@@ -20,17 +15,14 @@ namespace Bookstore.Web.Helpers
                 Path = "/"
             };
 
-            HttpCookie cookie = context.Request.Cookies[CookieKey];
-            string shoppingCartClientId = cookie != null ? cookie.Value : null;
-
-            //var shoppingCartClientId = context.Request.Cookies[CookieKey].Value;
+            string shoppingCartClientId = context.Request.Cookies[CookieKey];
 
             if (string.IsNullOrWhiteSpace(shoppingCartClientId))
             {
                 shoppingCartClientId = context.User.Identity.IsAuthenticated ? context.User.GetSub() : Guid.NewGuid().ToString();
             }
 
-            context.Response.Cookies.Add(new HttpCookie(CookieKey, shoppingCartClientId));
+            context.Response.Cookies.Append(CookieKey, shoppingCartClientId, cookieOptions);
 
             return shoppingCartClientId;
         }
